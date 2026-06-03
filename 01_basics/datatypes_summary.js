2e9f5df1192cb75d110138ae30002fab973ed825
satyamKr987 <114725073+satyamKr987@users.noreply.github.com>
2026-06-03 11:51:41 +0000

    datatypes Summary add

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,35 @@
+//primitive dataTypes
+
+// 7  :String, Number, Boolean, null,  undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol ('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+// const bigNumber = 34567876568n
+
+
+// Refrence DataTypes or Non Primitive
+
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "nagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp);
